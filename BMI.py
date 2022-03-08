f1c41e77726aa2a1f9a5b2af3b2628cc15347a09
Nicole Ogertschnig <60281511+MsNikki-85@users.noreply.github.com>
2022-03-07 18:58:09 -0700

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,11 @@
+#BMI calculator using inches and pounds
+# 🚨 Don't change the code below 👇
+height = input("enter your height in inches: ")
+weight = input("enter your weight in pounds: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = (int(weight) / float(height) **2) * 703
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
